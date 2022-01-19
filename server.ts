db6efee232f0af3887e07a3f090d3766419c5f3c
Muhammad Ahmad <181370123@gift.edu.pk>
2022-01-19 23:25:30 +0500

    add products route to fetch products from product table

--- server.ts
+++ server.ts
@@ -1,10 +1,17 @@
 import express, {Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const PORT = process.env.PORT || 3000
 const app = express()
+const prisma = new PrismaClient()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running')
+})
+
+app.get('/products', async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany()
+  res.json({ products })
 })
 
 app.listen(PORT, () => console.error(`Express Server running on http://localhost:${PORT}`))
